c32368f7994703b6ad86dc2a913b9083146e8470
ToMibukola <fathiat2223@gmail.com>
2023-01-16 22:47:32 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+var color = "pink";
+var name = 'Fathiat';
+let food = 'Rice';
+const myNumbers = "123456"
+let m = '1+2+3';
+
+console.log(color)
+console.log(myNumbers)
+
+
+let lenght = 20;
+let weight = 17.55;
+var height = -1567;
+const x = 0.005;
+let z = 20+17.55;
+
+console.log(height)
+console.log(z)
+
+
+let a = true;
+let b = false;
+const tomiIsBeautiful = true;
+var tomiIsTall = true;
+let tomiIsNice = false;
+
+console.log(a)
+console.log(tomiIsBeautiful)
+console.log(tomiIsNice)
+
+let c;
+let d;
+var bit = 20;
+const ten = 0;
+let s = b;
+
+console.log(c)
+console.log(d)
+
+let cow = null;
+let bigGrain = null;
+
+console.log(cow)
+console.log(bigGrain)
+
+
+let data = [12, 10, 'ade', false] ;
+let colours = ['pink', 20, -199, 09.2, "blue"];
+var shapes = ['triangle', false, 20000,];
+let eatables = ['rice',"beans", 10, -30];
+console.log(eatables)
+console.log(data)
+
+
+
+
+
+var classScore = {
+    ten:2, thirty:10, fiftyOne:11
+}
+console.log(classScore)
